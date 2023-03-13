587f53eaceabe9fc29578719610b0a842da8ebb6
kvbaurb99 <kvbaurbanski2@gmail.com>
2023-03-13 11:32:18 +0100

    fix

--- src/components/HeroInfo.jsx
+++ src/components/HeroInfo.jsx
@@ -21,7 +21,7 @@ export default function HeroInfo() {
     <div className='w-[80%] mx-auto flex h-[600px] items-center relative justify-center md:justify-start'>
         <div className='max-w-[440px] text-white relative product text-center md:text-start'>
             <p className='tracking-widest text-white/50 md:text-base text-xl'>NEW PRODUCT</p>
-            <p className='md:text-6xl  text-4xl font-bold tracking-widest mt-4'>XX99 MARK II HEADPHONES</p>
+            <p className='md:text-5xl  text-4xl font-bold tracking-widest mt-4'>XX99 MARK II HEADPHONES</p>
             <p className='md:mt-4 mt-6 tracking-wider max-w-[400px] text-sm md:text-base'>Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
             <Link to='/headphones/xx99-mark-two-headphones'><button className='bg-[#D87D4A] mt-8 md:mt-4 px-6 md:px-12 py-3 text-sm hover:bg-[#FBAF85]  duration-700'>SEE PRODUCT</button></Link>
         </div>
